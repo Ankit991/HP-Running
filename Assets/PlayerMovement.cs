974d414a3958079d334677cc733607f74c6706fa
Ankit991 <ankitrajpoot9696466@gmail.com>
2021-05-21 22:23:58 +0530

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public int movespeed,turnspeed;
+    StartFollow startfollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startfollow = GetComponent<StartFollow>();
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 playerpos = transform.position;
+        playerpos.x = Mathf.Clamp(transform.position.x, -6, 6);
+        transform.position = playerpos;
+
+
+        transform.Translate(Vector3.forward * movespeed*Time.deltaTime);
+        float x = Input.GetAxis("Horizontal");
+
+
+        transform.Translate(Vector3.right * x * turnspeed*Time.deltaTime);
+
+       
+        
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Follow")
+        {
+            startfollow.followplayer = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Follow")
+        {
+            startfollow.followplayer = false;
+        }
+    }
+
+}
---
+++ Assets/StartFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFollow : MonoBehaviour
+{
+
+    public Transform[] players;
+    public  bool followplayer;
+    public Transform followthis;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (followplayer)
+        {
+           
+            foreach (Transform pl in players)
+            {
+                if (Vector3.Distance(this.transform.position, pl.position) < 12f)
+                {
+                    pl.transform.parent = followthis;
+                   
+                }
+            }
+           
+          
+        }
+    }
+}
